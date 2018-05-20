eb443d20d448368f2e0d733760d6b694bc2a5c17
duydang0908 <38183058+duydang0908@users.noreply.github.com>
2018-05-20 16:52:56 +0700

    first

---
+++ Project/Project/Controllers/LibraryController.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Project;
+using PagedList;
+using PagedList.Mvc;
+
+namespace Project.Controllers
+{
+    public class LibraryController : Controller
+    {
+        QLTVEntities data = new QLTVEntities();
+
+        private QLTVEntities db = new QLTVEntities();
+
+        // GET: Library
+        public async Task<ActionResult> Index(int ?page)
+        {
+            //var sACHes = db.SACHes.Include(s => s.CHUDE).Include(s => s.LoaiSach).Include(s => s.NXB);
+            //return View(await sACHes.ToListAsync());
+            int pageSize = 5;
+            int pageNum = (page ?? 1);
+            var sachmoi = Laysachmoi(15);
+            return View(sachmoi.ToPagedList(pageNum, pageSize));
+        }
+
+        // GET: Library/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SACH sACH = await db.SACHes.FindAsync(id);
+            if (sACH == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sACH);
+        }
+
+        // GET: Library/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaCD = new SelectList(db.CHUDEs, "MaCD", "TenCD");
+            ViewBag.MaLoaiSach = new SelectList(db.LoaiSaches, "MaLoaiSach", "TenLoaiSach");
+            ViewBag.MaNXB = new SelectList(db.NXBs, "MaNXB", "TenNXB");
+            return View();
+        }
+
+        // POST: Library/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Masach,Tensach,MaLoaiSach,Mota,Hinhminhhoa,MaCD,MaNXB,Ngaycapnhat")] SACH sACH)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SACHes.Add(sACH);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaCD = new SelectList(db.CHUDEs, "MaCD", "TenCD", sACH.MaCD);
+            ViewBag.MaLoaiSach = new SelectList(db.LoaiSaches, "MaLoaiSach", "TenLoaiSach", sACH.MaLoaiSach);
+            ViewBag.MaNXB = new SelectList(db.NXBs, "MaNXB", "TenNXB", sACH.MaNXB);
+            return View(sACH);
+        }
+
+        // GET: Library/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SACH sACH = await db.SACHes.FindAsync(id);
+            if (sACH == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaCD = new SelectList(db.CHUDEs, "MaCD", "TenCD", sACH.MaCD);
+            ViewBag.MaLoaiSach = new SelectList(db.LoaiSaches, "MaLoaiSach", "TenLoaiSach", sACH.MaLoaiSach);
+            ViewBag.MaNXB = new SelectList(db.NXBs, "MaNXB", "TenNXB", sACH.MaNXB);
+            return View(sACH);
+        }
+
+        // POST: Library/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Masach,Tensach,MaLoaiSach,Mota,Hinhminhhoa,MaCD,MaNXB,Ngaycapnhat")] SACH sACH)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sACH).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaCD = new SelectList(db.CHUDEs, "MaCD", "TenCD", sACH.MaCD);
+            ViewBag.MaLoaiSach = new SelectList(db.LoaiSaches, "MaLoaiSach", "TenLoaiSach", sACH.MaLoaiSach);
+            ViewBag.MaNXB = new SelectList(db.NXBs, "MaNXB", "TenNXB", sACH.MaNXB);
+            return View(sACH);
+        }
+
+        // GET: Library/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SACH sACH = await db.SACHes.FindAsync(id);
+            if (sACH == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sACH);
+        }
+
+        // POST: Library/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            SACH sACH = await db.SACHes.FindAsync(id);
+            db.SACHes.Remove(sACH);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private List<SACH> Laysachmoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+
+        public ActionResult Chude()
+        {
+            var chude = from cd in data.CHUDEs select cd;
+            return PartialView(chude);
+        }
+
+        public ActionResult Nxb()
+        {
+            var nxb = from c in data.NXBs select c;
+            return PartialView(nxb);
+        }
+
+        public ActionResult sachTheochude(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+        public ActionResult sachTheoNXB(int id)
+        {
+            var sach = from s in data.NXBs where s.MaNXB == id select s;
+            return View(sach);
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            var tendn = collection["username"];
+            var mk = collection["pass"];
+            if (String.IsNullOrEmpty(tendn) || String.IsNullOrEmpty(mk))
+                ViewData["Loi2"] = "Phải điền đầy đủ thông tin";
+            else
+            {
+                DOCGIA dg = data.DOCGIAs.SingleOrDefault(n => n.Email == tendn && n.DienThoai == mk);
+                {
+                    if (dg != null)
+                    {
+                        ViewBag.Thongbao = "Đăng nhập thành công";
+                        Session["Taikhoan"] = dg.TenDG;
+                        return RedirectToAction("Index", "Library");
+                    }
+                    else
+                        ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                }
+            }
+            return View();
+        }
+        
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Library");
+        }
+
+    }
+}
---
+++ Project/Project/Controllers/OrderController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class OrderController : Controller
+    {
+        QLTVEntities data = new QLTVEntities();
+
+        public List<Order>Layorder()
+        {
+            List<Order> lstOrder = Session["Order"] as List<Order>;
+            if(lstOrder==null)
+            {
+                lstOrder = new List<Order>();
+                Session["Order"] = lstOrder;
+            }
+            return lstOrder;
+        }
+
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private int tongSoLuong()
+        {
+            int itongSL = 0;
+            List<Order> lstOrder = Session["Order"] as List<Order>;
+            if (lstOrder != null)
+                itongSL = lstOrder.Sum(n => n.iSL);
+            return itongSL;
+        }
+
+        public ActionResult ThemGioHang(int iMasach, string strURL)
+        {
+            List<Order> lstOrder = Layorder();
+            Order sp = lstOrder.Find(n => n.iMasach == iMasach);
+            if(sp==null)
+            {
+                sp = new Order(iMasach);
+                lstOrder.Add(sp);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sp.iSL++;
+                return Redirect(strURL);
+            }
+        }
+
+        public ActionResult GioHang()
+        {
+            List<Order> lstOrder = Layorder();
+            if (lstOrder.Count == 0)
+                return RedirectToAction("Index", "Library");
+            ViewBag.Tongsoluong = tongSoLuong();
+            return View(lstOrder);
+        }
+
+        public ActionResult GiohangPartial()
+        {
+            ViewBag.Tongsoluong = tongSoLuong();
+            return PartialView();
+        }
+
+        public ActionResult XoaGioHang(int iMaSP)
+        {
+            List<Order> lstOrder = Layorder();
+            Order sp = lstOrder.SingleOrDefault(n => n.iMasach == iMaSP);
+            if(sp!=null)
+            {
+                lstOrder.RemoveAll(n => n.iMasach == iMaSP);
+                return RedirectToAction("GioHang");
+            }
+            if (lstOrder.Count == 0)
+                return RedirectToAction("Index", "Library");
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult CapnhatGiohang(int iMaSP,FormCollection f)
+        {
+            List<Order> lstOrder = Layorder();
+            Order sp = lstOrder.SingleOrDefault(n => n.iMasach == iMaSP);
+            if (sp != null)
+                sp.iSL = int.Parse(f["txtSoluong"].ToString());
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult XoaTatcaGiohang()
+        {
+            List<Order> lstOrder = Layorder();
+            lstOrder.Clear();
+            return RedirectToAction("Index", "Library");
+        }
+        
+    }
+}
